cb9f11d269e51d4d0e732e3151b077bf3ad38433
Simon LANOS <simon.lanos99@gmail.com>
2018-06-18 13:38:30 +0200

    feat: ajoute des fichiers de template

---
+++ Includes/Templates/article-minimal.php
@@ -0,0 +1,11 @@
+<article>
+    <div class="art-contender">
+        <h1>[title]</h1>
+        <h2>[sub-title]</h2>
+        <img src="./assets/img/[img].jpg" alt="Image de montagne" class="art-img">
+        <hr>
+        <p class="art-content">
+            [content]
+        </p>
+    </div>
+</article>
---
+++ Includes/Templates/article.php
@@ -0,0 +1,10 @@
+<article>
+    <div class="art-contender">
+        <h1>[title]</h1>
+        <img src="./assets/img/[img].jpg" alt="Image de montagne" class="art-img">
+        <hr>
+        <p class="art-content">
+            [content]
+        </p>
+    </div>
+</article>
